e8a0743ab805c5f05e623db4d28fa8a17439b239
fimashagal <fimashagal@gmail.com>
2018-08-30 23:01:02 +0300

    update

--- js/index.js
+++ js/index.js
@@ -26,27 +26,27 @@
             0: {
                 image: "wealth-0",
                 scoreProfit: 10,
-                scoreDamage: width === 768 ? -3 : -5
+                scoreDamage: width === 768 ? -4 : -5
             },
             1: {
                 image: "wealth-1",
                 scoreProfit: 12,
-                scoreDamage: width === 768 ? -4 : -6
+                scoreDamage: width === 768 ? -5 : -6
             },
             2: {
                 image: "wealth-2",
                 scoreProfit: 14,
-                scoreDamage: width === 768 ? -5 : -7
+                scoreDamage: width === 768 ? -6 : -7
             },
             3: {
                 image: "wealth-3",
                 scoreProfit: 16,
-                scoreDamage: width === 768 ? -6 : -8
+                scoreDamage: width === 768 ? -7 : -8
             },
             4: {
                 image: "wealth-4",
                 scoreProfit: 18,
-                scoreDamage: width === 768 ? -7 : -9
+                scoreDamage: width === 768 ? -8 : -9
             }
         };
 
