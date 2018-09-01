61a3ef6b26d21eca351f289ca82ed2d0f4bc4e99
fimashagal <fimashagal@gmail.com>
2018-09-01 18:20:29 +0300

    improve physics

--- js/index.js
+++ js/index.js
@@ -4,15 +4,21 @@
     swInit();
 
     function dbInit(callback = new Function) {
-        localforage.getItem('wealthfall')
-            .then(function(value) {
-                !value
-                    ? localforage.setItem('wealthfall', 0).then(callback)
-                    : callback(value);
-            }).catch(function(err) {
+        try {
+            localforage.getItem('wealthfall')
+                .then(function(value) {
+                    !value
+                        ? localforage.setItem('wealthfall', 0).then(callback)
+                        : callback(value);
+                }).catch(function(err) {
                 console.log(err);
                 callback(0);
             });
+        } catch (err) {
+            console.warn(err);
+            callback(0);
+        }
+
     }
 
     function swInit() {
